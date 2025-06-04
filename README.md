c124161a58e345f23443004a365eae6a3e6983ba
Jay <211176133+Sprinkler3679@users.noreply.github.com>
2025-06-04 00:33:44 -0500

    Add function to create an array of prompts in main.js. Add skills used to README.md

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Story Prompt Message Generator
 
-This is a Codecademy project using JavaScript and Git version control to build a message generator program that uses at least three separate data inputs to create a message.
+This is a Codecademy project using JavaScript and Git version control to build a message generator that uses at least three separate data inputs to create a message.
+
+## JavaScript Skills Used
+
+- Create Functions
+- Create Factory with getters and method(s)
+- Use if/else statement to modify a word depending on if it starts with a vowel or not
+- Use For Loops to create an array of story prompt objects and to iterate through the array of prompt objects to display the generated prompts
+
+## Git Skills Used
+
+- Create a repository
+- Add files
+- Check status of staged files
+- Commit changes
+- Amend a commit
+- Create branch
+- Remove branch (new code was inadequate and abandoned)
+- Push code to Github
--- main.js
+++ main.js
@@ -4,10 +4,12 @@ const subjects = ["cat", "widow", "vampire", "hiker", "scout troop", "young man"
     "sorceress", "wizard","engineer", "artist", "actor", "pirate", "sailor"];
 
 const verbs = ["finds", "hunts", "seeks", "fights", "chases", "is chased by", "faces", "feeds", 
-    "hears", "dreams about", "stares down", "visits", "is ambushed by", "sings to", "dances with"];
+    "hears", "dreams about", "stares down", "visits", "is ambushed by", "sings to", "dances with",
+    "conquers"];
 
 const objects = ["a lion", "a werewolf", "a raging storm", "a pack of wolves", "an unseen horror", 
-    "an inner demon", "an angry mob", "a ferocious beast", "a doppleganger", "a litter of kittens"];
+    "an inner demon", "an angry mob", "a ferocious beast", "a doppleganger", "a litter of kittens",
+    "a vampire", "a pack of zombies", "a band of thieves"];
 
 const locations = ["on a grassy plain", "deep in the forest", "on a winding mountain road", 
     "in the depths of a cave", "inside a house", "on a lake", "on the beach", "on a ship", 
@@ -19,7 +21,7 @@ function randomWord(array) {
     return array[i];
 }
 
-// Starts the sentence with "A [subject]" / "An [subject]" appropriate for a vowel or consonant first subject letter.
+// Starts the sentence with "A [subject]" / "An [subject]" dependent on whether the first subject letter is a vowel or not
 function intro() {
     let beginning = randomWord(subjects);
     let vowels = ['a', 'e', 'i', 'o', 'u'];
@@ -32,42 +34,45 @@ function intro() {
 // Creates a story prompt object using a supplied subject, verb, object, and location
 const promptFactory = (_subject, _verb, _object, _location) => {
     return {
-    _subject,
-    _verb,
-    _object,
-    _location,
+        _subject,
+        _verb,
+        _object,
+        _location,
 
-    // Getters are supplied for filtering and sorting funtionality in the future
-    get subject() {
-        return this._subject;
-    },
-    get verb() {
-        return this._verb;
-    },
-    get object() {
-        return this._object;
-    },
-    get location() {
-        return this._location;
-    },
+        // Getters are supplied for filtering and sorting funtionality in the future
+        get subject() {
+            return this._subject;
+        },
+        get verb() {
+            return this._verb;
+        },
+        get object() {
+            return this._object;
+        },
+        get location() {
+            return this._location;
+        },
 
-    // Method to return a complete story prompt
-    revealPrompt() {
-        return `${this._subject} ${this._verb} ${this._object} ${this._location}`;
-    },}    
+        // Method to return a complete story prompt
+        revealPrompt() {
+            return `${this._subject} ${this._verb} ${this._object} ${this._location}`;
+        },
+    }    
 }
-
-
-// Choose number of story prompts generated here:
-let numPromptsGenerated = "5";
 
 // Create an array of prompts
-let promptsCollection = [];
-for (let i = 0; i < numPromptsGenerated; i++) {
-    promptsCollection.push(promptFactory(intro(), randomWord(verbs), randomWord(objects), randomWord(locations)));
+function createPromptsCollection(numPromptsGenerated) {
+    let newPromptArray = [];
+    for (let i = 0; i < numPromptsGenerated; i++) {
+        newPromptArray.push(promptFactory(intro(), randomWord(verbs), randomWord(objects), randomWord(locations)));
+    }
+    return newPromptArray;
 }
 
+// Store a generated array containing the specified number of prompts
+let promptsCollection = createPromptsCollection( 3 );
+
 // Display each of the generated prompts
-for (let i = 0; i < numPromptsGenerated; i++) {
+for (let i = 0; i < promptsCollection.length; i++) {
     console.log(promptsCollection[i].revealPrompt()); 
 } 
